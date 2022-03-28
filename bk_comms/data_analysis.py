bb5d8a81e1ce3ea33eaa51f7bd60c4b6594d95c3
Behzad Karkaria <bezk@dhcp-10-240-159-138.cp.wireless.private.cam.ac.uk>
2022-03-28 13:42:00 +0100

    added toxin mat plots

--- bk_comms/data_analysis.py
+++ bk_comms/data_analysis.py
@@ -28,11 +28,12 @@ colours = [
 class DataAnalysis:
     def __init__(
         self,
-        experiment_dir="./output/mel_indiv_growth/exp_B_longum_subsp_longum_ga_fit_batch_tests",
+        experiment_dir,
+        particle_pickle_regex,
         data_path="./experimental_data/mel_target_data/mel_indiv_target_data.csv",
     ):
         self.experiment_dir = experiment_dir
-        self.run_directories = glob(experiment_dir + "/run_*/")
+        self.run_directories = glob(particle_pickle_regex)
         self.config = self.read_omega_config()
         self.particles = self.load_all_particles(self.run_directories)
 
@@ -45,7 +46,7 @@ class DataAnalysis:
     def read_omega_config(self):
         # We are assuing all included run directories
         # have the same config (dangerous)
-        conf = OmegaConf.load(self.run_directories[0] + "/cfg.yaml")
+        conf = OmegaConf.load(self.experiment_dir + "run_1/cfg.yaml")
 
         return conf["cfg"]
 
@@ -71,12 +72,19 @@ class DataAnalysis:
 
         return filtered_particles
 
-    def load_all_particles(self, repeat_dirs):
+    def load_all_particles(self, particle_paths):
         all_particles = []
-        for d in repeat_dirs:
-            particle_paths = glob(f"{d}particles_*.pkl")
-            for p_path in particle_paths:
-                all_particles.extend(self.load_pickle(p_path))
+        for p_path in particle_paths:
+            particle = self.load_pickle(p_path)
+
+            try:
+                del particle.max_exchange_prior
+                del particle.k_val_prior
+                del particle.toxin_interaction_prior
+
+            except:
+                pass
+            all_particles.extend(self.load_pickle(p_path))
 
         return all_particles
 
@@ -104,7 +112,7 @@ class DataAnalysis:
         k_val_cols = []
         lb_constraint_cols = []
         toxin_cols = []
-        
+
         for m in init_particle.model_names:
             # Make column headings
             k_val_cols += [f"K_{x}_{m}" for x in init_particle.dynamic_compounds]
@@ -112,17 +120,18 @@ class DataAnalysis:
                 f"lb_constr_{x}_{m}" for x in init_particle.dynamic_compounds
             ]
 
-        
         # Make toxin interaction headings
         for donor_model in init_particle.model_names:
             for recipient_model in init_particle.model_names:
-                toxin_cols += [
-                    f"toxin_{donor_model}_{recipient_model}"
-                ]
+                toxin_cols += [f"toxin_{donor_model}_{recipient_model}"]
 
         column_headings = []
         column_headings += (
-            init_conc_cols + init_species_col + k_val_cols + lb_constraint_cols + toxin_cols
+            init_conc_cols
+            + init_species_col
+            + k_val_cols
+            + lb_constraint_cols
+            + toxin_cols
         )
 
         parameters = np.zeros([len(particles), len(column_headings)])
@@ -175,23 +184,22 @@ class DataAnalysis:
     def plot_paerto_front(self):
         distances = self.get_particle_distances(self.particles)
         ndf, dl, dc, ndr = pygmo.fast_non_dominated_sorting(distances)
-        
+
         front_1_distances = distances[ndf[0]]
 
         for idx_i, name_i in enumerate(self.particles[0].model_names):
-         for idx_j, name_j in enumerate(self.particles[0].model_names):
-            if idx_i == idx_j:
-                continue
-            
-            fig = px.scatter(x=front_1_distances[:, idx_i], y=front_1_distances[:, idx_j])
-            fig.update_xaxes(
-            title_text = name_i)
+            for idx_j, name_j in enumerate(self.particles[0].model_names):
+                if idx_i == idx_j:
+                    continue
 
-            fig.update_yaxes(
-                    title_text = name_j)
+                fig = px.scatter(
+                    x=front_1_distances[:, idx_i], y=front_1_distances[:, idx_j]
+                )
+                fig.update_xaxes(title_text=name_i)
 
-            fig.write_image(f"{self.output_dir}/pareto_front_{idx_i}_{idx_j}.png")
+                fig.update_yaxes(title_text=name_j)
 
+                fig.write_image(f"{self.output_dir}/pareto_front_{idx_i}_{idx_j}.png")
 
     def plot_population_timeseries(self, plot_n_particles=10):
         n_sol_keys = len(self.config["exp_sol_keys"])
@@ -413,6 +421,26 @@ class DataAnalysis:
             # fig.update_yaxes(title="Abundance", type='log')
             fig.write_image(f"{self.output_dir}/abundance_bar_particle_{p_idx}.png")
 
+    def plot_toxin_interaction_matricies(self):
+        for p_idx, p in enumerate(self.particles):
+            print(p.toxin_mat)
+            fig = make_subplots(rows=1, cols=1, shared_xaxes=True, shared_yaxes="all")
+
+            population_names = [pop.name for pop in p.populations]
+
+            heatmap = go.Heatmap(
+                x=list(range(len(population_names))),
+                y=list(range(len(population_names))),
+                z=p.toxin_mat,
+                colorscale="inferno",
+            )
+
+            heatmap["x"] = population_names
+            heatmap["y"] = population_names
+
+            fig.add_trace(heatmap)
+            fig.update_layout(template="simple_white", title='Toxin interaction mat')
+            fig.write_image(f"{self.output_dir}/toxin_mat_{p_idx}.png")
 
 
 def vis_multi():
@@ -420,37 +448,48 @@ def vis_multi():
         "/Users/bezk/Documents/CAM/research_code/yeast_LAB_coculture/output/mel_mixes_growth/mel_multi_mix2_m2_growers_test/"
     ]
 
-    data_directories = ['/Users/bezk/Documents/CAM/research_code/yeast_LAB_coculture/output/mel_mixes_growth/mel_multi_mix2_m2_growers/generation_0/',
-    '/Users/bezk/Documents/CAM/research_code/yeast_LAB_coculture/output/mel_mixes_growth/mel_multi_mix2_m2_growers/generation_1/',
-    '/Users/bezk/Documents/CAM/research_code/yeast_LAB_coculture/output/mel_mixes_growth/mel_multi_mix2_m2_growers/generation_2/']
+    data_directories = [
+        "/Users/bezk/Documents/CAM/research_code/yeast_LAB_coculture/output/mel_mixes_growth/mel_multi_mix2_m2_growers/generation_9/"
+    ]
 
     mix_target_data_path = "/Users/bezk/Documents/CAM/research_code/yeast_LAB_coculture/experimental_data/mel_target_data/target_data_pH7_Mix2_Med2.csv"
 
     target_data_path = mix_target_data_path
 
     for x in data_directories:
-        d = DataAnalysis(experiment_dir=x, data_path=target_data_path)
-        # d.particles = d.filter_particles_by_distance([100.0, 0.2, 0.2, 0.5, 0.25, 100.0])
-        d.plot_paerto_front()
+        particle_path_regex = f"{x}/run_*/*.pkl"
+        d = DataAnalysis(
+            experiment_dir=x,
+            particle_pickle_regex=particle_path_regex,
+            data_path=target_data_path,
+        )
 
         distances = d.get_particle_distances(d.particles)
-        ndf, dl, dc, ndr = pygmo.fast_non_dominated_sorting(distances)
+        sum_distances = [sum(d) for d in distances]
 
-        d.particles = [ d.particles[idx]  for idx in ndf[0]]
+        sorted_particles = sorted(
+            d.particles, key=lambda x: sum_distances[d.particles.index(x)]
+        )
+        d.particles = sorted_particles[:10]
+        d.plot_toxin_interaction_matricies()
+
+        # ndf, dl, dc, ndr = pygmo.fast_non_dominated_sorting(distances)
+
+        # d.particles = [ d.particles[idx]  for idx in ndf[0]]
 
         for p in d.particles:
             print("tox_sum", p.toxin_mat.sum())
-            print(p.distance)
+            print(sum(p.distance))
 
-
-        d.plot_tsne(
-            include_init_metabolites=False,
-            include_lb_constraints=True,
-            include_k_values=False,
-            include_init_species=False,
-        )
+        # d.plot_tsne(
+        #     include_init_metabolites=False,
+        #     include_lb_constraints=True,
+        #     include_k_values=False,
+        #     include_init_species=False,
+        # )
 
         d.plot_abundance_bar()
+        d.plot_toxin_interaction_matricies()
         d.plot_distance_distributions()
         d.plot_population_timeseries(plot_n_particles=100)
         d.plot_fold_change_timeseries(plot_n_particles=100)
