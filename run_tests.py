8349ad88ff637ae471489c530942b13f3289f9a8
Behzad Karkaria <bezk@Behzads-MacBook-Pro.local>
2022-09-26 09:47:40 +0100

    updated tests

--- run_tests.py
+++ run_tests.py
@@ -1,21 +1,19 @@
-from omegaconf import DictConfig, open_dict
+from omegaconf import DictConfig
 
-from hydra import compose, initialize
 from omegaconf import OmegaConf
-import psutil
-import os
 import hydra
 from hydra.utils import instantiate
 from pathlib import Path
-from hydra import initialize, compose
 import functools
 
 from loguru import logger
 
-import bk_comms
 
-from bk_comms.sampling import SampleCombinationParticles
-from bk_comms.data_analysis.visualisation import pipeline as vis_pipeline
+from paragem.sampling import SampleCombinationParticles
+from paragem.data_analysis.visualisation import pipeline as vis_pipeline
+from paragem.utils import save_particles
+from paragem.data_analysis.visualisation import prepare_particles_df
+
 
 def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
     def wrapper(func):
@@ -25,12 +23,10 @@ def logger_wraps(*, entry=True, exit=True, level="DEBUG"):
         def wrapped(*args, **kwargs):
             logger_ = logger.opt(depth=1)
             if entry:
-                logger_.log(
-                    level, "Entering '{}' (args={}, kwargs={})", name, args, kwargs
-                )
+                logger_.log(level, "Entering '{}'", name)
             result = func(*args, **kwargs)
             if exit:
-                logger_.log(level, "Exiting '{}' (result={})", name, result)
+                logger_.log(level, "Exiting '{}'", name)
             return result
 
         return wrapped
@@ -49,22 +45,26 @@ def test_main(cfg: DictConfig):
     simulator = test_instantiate_simulator(cfg)
 
     alg = test_instantiate_algorithm(cfg)
-
-
     particles = test_gen_initial_population(alg, cfg)
 
-    test_calculate_particle_distances(alg, particles, 'M3')
+    test_calculate_particle_distances(alg, particles, "M3")
     test_save_particles(alg, particles)
     test_load_particles(alg)
     test_combination_particle_sampler(alg)
 
+    particles_df = prepare_particles_df(cfg)
 
-    vis_pipeline(cfg)
+    visualisation_pipeline = cfg.visualisation
 
+    for step in visualisation_pipeline:
+        print(step)
+        step = instantiate(step, particles_df=particles_df)
 
 
 @logger_wraps()
-def test_particle_simulation(simulator, media_name, particles, n_processes=2, parallel=True):
+def test_particle_simulation(
+    simulator, media_name, particles, n_processes=2, parallel=True
+):
     """
     Test the particle simulation
     """
@@ -110,13 +110,13 @@ def test_save_particles(alg, particles):
     """
     Test the load and save of particles
     """
-    alg.save_particles(
+
+    save_particles(
         particles,
+        alg.sim_media_names,
         output_dir=f"{alg.output_dir}",
     )
 
-
-@logger_wraps()
 def test_load_particles(alg):
     """
     Test the load and save of particles
@@ -127,7 +127,6 @@ def test_load_particles(alg):
     )
 
 
-@logger_wraps()
 def test_combination_particle_sampler(alg):
     """
     test combination particle sampler
