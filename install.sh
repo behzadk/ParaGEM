d67b5ec4d40bacefdde0359db76fcc6b6406bf42
Behzad Karkaria <bezk@Behzads-MacBook-Pro.local>
2022-09-26 09:47:08 +0100

    changes to install

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 source ~/.bashrc
 eval "$(conda shell.bash hook)"
-conda create -n "cobrapy_env" python=3.8.0 ipython
+conda create -n "paragem" python=3.8.0 ipython
 
-conda activate cobrapy_env
+conda activate paragem
+
 conda install -y scipy 
 conda install -y cobra
 conda install -y numba
