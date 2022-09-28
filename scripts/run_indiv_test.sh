973d6dbac2e4784edd221dafb6c9c5a1184553e9
Behzad Karkaria <bezk@behzads-mbp.home>
2022-09-28 10:43:46 +0100

    config changes and ponomarova models

--- scripts/run_indiv_test.sh
+++ scripts/run_indiv_test.sh
@@ -11,13 +11,13 @@ python -u run_tests.py  --config-name=template_indiv_NSGAII user=local \
 model_names.0=E_coli_IAI1 \
 run_idx=0 \
 data="M2_M3" \
-experiment_name=test_indiv \
+experiment_name=test_indiv_3 \
 algorithm.generation_idx=0 \
 algorithm.hotstart_particles_regex=null \
 algorithm.max_generations=1 \
-algorithm.population_size=25 \
+algorithm.population_size=50 \
 filter.min_growth.0='0.01' \
 filter.max_growth.0='10.0' \
 n_processes=5 \
-algorithm.n_particles_batch=5
+algorithm.n_particles_batch=8
 
